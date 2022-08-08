fd4a1783f9ec6215816f2d2e3bc7a9cc047aad6f
Arpan Sarkar <arpan98sarkar@gmail.com>
2022-08-09 00:28:41 +0530

    installed tailwind added basic components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'clips';
+  @ViewChild('videoRef', { static: true }) videoRef!: ElementRef;
+
+  ngOnInit() {
+  }
+
+  ngAfterViewInit(): void {
+    const media = this.videoRef.nativeElement
+    media.muted = true
+    media.play()
+  }
+
 }
---
+++ src/app/user/user.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { AuthModalComponent } from './auth-modal/auth-modal.component';
+
+@NgModule({
+  declarations: [
+    AuthModalComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    AuthModalComponent
+  ]
+})
+export class UserModule { }
