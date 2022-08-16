c0f04da9b1867e7ba13163c29ade742a6b054ea1
Arpan Sarkar <arpan98sarkar@gmail.com>
2022-08-16 23:34:37 +0530

    Services changes

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { SharedModule } from '../shared/shared.module';
 import { AuthModalComponent } from './auth-modal/auth-modal.component';
 
 @NgModule({
@@ -6,6 +7,7 @@ import { AuthModalComponent } from './auth-modal/auth-modal.component';
     AuthModalComponent
   ],
   imports: [
+    SharedModule
   ],
   exports: [
     AuthModalComponent
